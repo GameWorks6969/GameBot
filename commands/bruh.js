0661ae440dd599655a3da4509018dee0079cc70b
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-04-06 19:11:52 +0530

    Delete commands directory

--- commands/bruh.js
+++
@@ -1,8 +0,0 @@
-exports.run = async (bot,message,args) => {
-      message.channel.send("BRUH")
-      message.channel.send("by - 🤖")
-    }
-
-exports.help = {
-    name: 'bruh'
-}
--- commands/lenny.js
+++
@@ -1,8 +0,0 @@
-exports.run = async (bot,message,args) => {
-message.delete();
-		message.channel.send('( ͡° ͜ʖ ͡°)');
-}
-
-exports.help = {
-    name: 'lenny'
-}
