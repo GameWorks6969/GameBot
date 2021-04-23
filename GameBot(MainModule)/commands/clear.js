29d1031a5b383fa63e839fc573e875cc28a79198
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-04-23 06:51:12 +0530

    Delete GameBot(MainModule) directory

--- GameBot(MainModule)/commands/clear.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (bot,message,args) => {
-      let num = parseInt(message.content.replace('$!clear',''));
-      message.channel.bulkDelete(num + 1);
-      message.channel.send(`I have deleted ${num} messages!`).then(msg => msg.delete({ timeout:2000}))
-    }
-
-exports.help = {
-    name: 'clear'
-}
