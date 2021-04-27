a0b6cd9024da67fd05a0d4507a39b81c0d150dcc
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-04-27 07:37:54 +0530

    Update ban.js

--- GameBotMain/commands/moderation/ban.js
+++ GameBotMain/commands/moderation/ban.js
@@ -1,24 +1,34 @@
 const discord = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     info: {
         name: "ban",
         description: "Ban someone through GameBot!",
         usage: "@user",
-        aliases: ["There are no aliases!"]
+        aliases: ["There are no aliaases!"]
     },
 
     run: async function(client, message, args){
-   if(message.member.hasPermission("ADMINSTRATOR")) {
-        let member = message.mentions.members.first()
-        if (!member) message.channel.send("Please mention someone")
-        else {
-          member.ban().then(mem => {
-            message.channel.send(`Banned ${mem.user.username}`)
-          })
+    let member = message.mentions.members.first()
+        if(message.member.hasPermission("ADMINSTRATOR")) {
+    if (!member) message.channel.send("Please mention a user or provide a valid user ID! (or *else*)");
+    if (member === message.member) 
+      return message.channel.send('You cannot ban yourself lol'); 
+    
+    await member.ban({ reason: 'BIG oof.' });
+
+    const embed = new MessageEmbed()
+      .setTitle('Ban Member!')
+      .setDescription(`${member} was successfully banned!`)
+      .addField('Moderator', message.member)
+      .addField('Member', member)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor('RED');
+    message.channel.send(embed);   
+}else{
+          message.channel.send('You do not have the ***perms chemicals***')
         }
-      }else {
-        message.reply("You don't have permisson to ban people")
-      } 
    }
 }
