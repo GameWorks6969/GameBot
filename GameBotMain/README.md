e436cedc4ae72bb75cc6d2b17fb8b800a964edec
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-05-14 22:23:22 +0530

    Delete GameBotMain directory

--- GameBotMain/README.md
+++
@@ -1,2 +0,0 @@
-# GameBotMusic (.env) instructions
-Rename the gbm.env file to - '.env' then open the file and put your bot token in the TOKEN="YOUR_TOKEN_HERE" part and your prefix in PREFIX="YOUR_TOKEN_HERE"
--- GameBotMain/events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  console.log(`${client.user.username} is online on ${client.guilds.cache.size} servers!`);
-};
