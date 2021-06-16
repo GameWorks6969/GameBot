50a33ee54d498a9e4cfc0eb906bd4c4c93c58ae1
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-06-16 13:44:13 +0530

    Delete GameBot(MusicModue) directory

--- GameBot(MusicModue)/README.md
+++
@@ -1,2 +0,0 @@
-# GameBotMusic (.env) instructions
-Rename the -gb.env file to - '.env' then open the file and put your bot token in the TOKEN="your_token_here" part and your prefix in PREFIX="$%"
--- GameBot(MusicModue)/events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Over GameWorkYT!", {
-    type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
